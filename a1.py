9b8b855cbb7c821e8fabd6b0274e99c9807d081e
Rishal-Jr <rmrazni@gmail.com>
2024-12-31 14:53:23 +0530

    jjn

---
+++ a1.py
@@ -0,0 +1,10 @@
+answer=input("When was the first known use of the quiz?")
+if answer=="1781":
+    print("Correct")
+else:
+    print("The correct answer is '1781',not",answer)
+answer=input("Which built-in function can get information from the user?")
+if answer=="input":
+    print("Correct")
+else:
+    print("The correct answer is 'input',not",answer)
